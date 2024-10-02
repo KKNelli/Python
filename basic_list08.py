b5fb47fdf8dc04b686340784ac0ae621a70efe6c
KKNelli <sze4komkor@vasvari.org>
2024-10-02 19:37:01 +0200

    okt 2

--- basic_list08.py
+++ basic_list08.py
@@ -1,6 +1,16 @@
 # Készíts egy programot, melyben egy listát hozzol létre egész számokkal.
 # Számolk ki a lista páros illetve páratlan elemeinek az összegét és írasd ki az eredményeket.
-# Készíts két különböző megoldást
+# Készíts két különböző megoldást!
 
-lista = lista.range(0,11)
-print(lista)
+egesz_szamok = list(range(11))
+paros_osszeg = 0
+paratlan_osszeg = 0
+for x in egesz_szamok:
+    if x % 2 ==0:
+        paros_osszeg += x
+    else:
+        paratlan_osszeg += x
+print(f"A páros számok összege: {paros_osszeg}")
+print(f"A páratlan számok összege: {paratlan_osszeg}")
+
+
